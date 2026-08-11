60426f1ddb0ce9ae5b0ab97f763d8efe9c3cf1c7
Andrew Malone <andrew.malone@backmarket.com>
2026-08-11 09:35:44 +0200

    Fix Revolve icon rendering on GitHub Pages

--- app/components/RevIcon.vue
+++ app/components/RevIcon.vue
@@ -4,8 +4,23 @@ const props = defineProps<{ name: string; class?: string }>()
 const runtimeConfig = useRuntimeConfig()
 const baseHref = (runtimeConfig.app.baseURL ?? '/').replace(/\/$/, '')
 const src = computed(() => `${baseHref}/icons/${props.name}.svg`)
+
+const maskStyle = computed(() => ({
+  maskImage: `url(${src.value})`,
+  WebkitMaskImage: `url(${src.value})`,
+  maskRepeat: 'no-repeat',
+  WebkitMaskRepeat: 'no-repeat',
+  maskPosition: 'center',
+  WebkitMaskPosition: 'center',
+  maskSize: 'contain',
+  WebkitMaskSize: 'contain',
+}))
 </script>
 
 <template>
-  <img :src="src" :class="$props.class" aria-hidden="true" />
+  <span
+    :class="['inline-block shrink-0 bg-current', $props.class]"
+    :style="maskStyle"
+    aria-hidden="true"
+  />
 </template>
